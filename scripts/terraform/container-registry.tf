fd570710906959cbe65bec556fe79e8fa46bc58f
tharinduRana97 <tharindu.ranasinghe.official@gmail.com>
2024-09-21 12:46:13 +1000

    initial commit

--- scripts/terraform/container-registry.tf
+++ scripts/terraform/container-registry.tf
@@ -1,7 +1,25 @@
-resource "aws_ecr_repository" "ecr" {
-  name                 = var.container_registry
+# Create ECR repository for book_catalog
+resource "aws_ecr_repository" "book_catalog" {
+  name                 = "${var.container_registry}-book_catalog"
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = true
+  }
+
+  tags = {
+    Name = "${var.container_registry}-book_catalog"
+  }
+}
+
+# Create ECR repository for inventory_management
+resource "aws_ecr_repository" "inventory_management" {
+  name                 = "${var.container_registry}-inventory_management"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name = "${var.container_registry}-inventory_management"
   }
 }
