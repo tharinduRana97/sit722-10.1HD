6b3d96d400d7cc5bc297d02049cce66f869e6453
tharinduRana97 <tharindu.ranasinghe.official@gmail.com>
2024-09-22 07:38:55 +1000

    test 3

--- scripts/terraform/kubernetes-cluster.tf
+++ scripts/terraform/kubernetes-cluster.tf
@@ -21,7 +21,7 @@ module "eks" {
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.public_subnets
-  control_plane_subnet_ids = module.vpc.public_subnets
+  control_plane_subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_group_defaults = {
     ami_type       = "AL2_x86_64"
