fe3794fcc8f43a3cdcfe807ba757aa1d0095584c
tharinduRana97 <tharindu.ranasinghe.official@gmail.com>
2024-09-21 23:47:11 +1000

    test 3

--- scripts/terraform/vpc.tf
+++ scripts/terraform/vpc.tf
@@ -11,6 +11,12 @@ module "vpc" {
   public_subnets  = var.public_subnets
 
 
+  enable_nat_gateway = true
+
+  # Ensure that public subnets automatically assign public IPs
+  map_public_ip_on_launch = true
+
+
   public_subnet_tags = {
     "kubernetes.io/role/elb" = 1
   }
