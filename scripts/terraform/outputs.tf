541437abeb9b0d3ec6b0e42681a448d115c87e04
tharinduRana97 <tharindu.ranasinghe.official@gmail.com>
2024-09-21 11:25:33 +1000

    initial commit

--- scripts/terraform/outputs.tf
+++ scripts/terraform/outputs.tf
@@ -5,8 +5,9 @@ output "eks_cluster_name" {
 }
 
 output "ecr_repository_url" {
-  value = aws_ecr_repository.ecr.repository_url
+  value = local.ecr_repository_exists ? data.aws_ecr_repository.existing.repository_url : (aws_ecr_repository.ecr[0].repository_url)
 }
+
 
 output "vpc_id" {
   value = aws_vpc.vpc.id
