3fddd388a76c087de16ca6d5a30f3f075cbc2b43
tharinduRana97 <tharindu.ranasinghe.official@gmail.com>
2024-09-21 11:37:32 +1000

    initial commit

--- scripts/terraform/container-registry.tf
+++ scripts/terraform/container-registry.tf
@@ -1,20 +1,7 @@
-# Check if the ECR repository already exists
-data "aws_ecr_repository" "existing" {
-  name = var.container_registry
-}
-
-# Determine whether the ECR repository needs to be created
-locals {
-  ecr_repository_exists = length(try([data.aws_ecr_repository.existing.repository_url], [])) > 0
-}
-
-# Create ECR repository only if it doesn't exist
 resource "aws_ecr_repository" "ecr" {
-  count = local.ecr_repository_exists ? 0 : 1
-  name  = var.container_registry
+  name                 = var.container_registry
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = true
   }
 }
-
