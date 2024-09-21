af9d8fb85615954d9f997d488e50bbba17cad382
tharinduRana97 <tharindu.ranasinghe.official@gmail.com>
2024-09-21 13:45:15 +1000

    second commit

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -4,4 +4,14 @@ set -u # or set -o nounset
 : "$NAME"
 : "$DIRECTORY"
 
-docker build -t $CONTAINER_REGISTRY/$NAME:$VERSION --file ./$DIRECTORY/Dockerfile ./$DIRECTORY
+# Construct the full repository URL by appending the microservice name to the base registry URL
+FULL_REPOSITORY_URL="$CONTAINER_REGISTRY-$NAME"
+
+# Construct the full image tag with the repository URL and version
+FULL_IMAGE_TAG="$FULL_REPOSITORY_URL:$VERSION"
+
+# Build and tag the Docker image directly with the full image tag
+docker build -t $FULL_IMAGE_TAG --file ./$DIRECTORY/Dockerfile ./$DIRECTORY
+
+# Output the full image tag for reference or use in other scripts
+echo "Image built and tagged as: $FULL_IMAGE_TAG"
--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -6,12 +6,21 @@ set -u # or set -o nounset
 : "$VERSION"
 : "$NAME"
 
+# Construct the full repository URL by appending the microservice name to the base registry URL
+FULL_REPOSITORY_URL="$CONTAINER_REGISTRY-$NAME"
+
+# Construct the full image tag with the repository URL and version
+FULL_IMAGE_TAG="$FULL_REPOSITORY_URL:$VERSION"
+
 # Log in to Amazon ECR
-# This uses AWS CLI to get the login password and log into the Docker registry
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $CONTAINER_REGISTRY
 
-# Tag the Docker image
-docker tag $NAME:$VERSION $CONTAINER_REGISTRY/$NAME:$VERSION
+# Tag the Docker image with the appropriate full image tag for ECR
+# Assuming the image is already built locally with a local tag, e.g., NAME:VERSION
+docker tag $NAME:$VERSION $FULL_IMAGE_TAG
 
-# Push the Docker image to the repository
-docker push $CONTAINER_REGISTRY/$NAME:$VERSION
+# Push the Docker image to the ECR repository
+docker push $FULL_IMAGE_TAG
+
+# Output the full image tag for reference
+echo "Image tagged and pushed to: $FULL_IMAGE_TAG"
