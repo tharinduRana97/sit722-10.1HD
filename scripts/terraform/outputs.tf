ecbe3b8705ae4101122782e21ff75fd6fe4a0e59
tharinduRana97 <tharindu.ranasinghe.official@gmail.com>
2024-09-21 12:59:10 +1000

    initial commit

--- scripts/terraform/outputs.tf
+++ scripts/terraform/outputs.tf
@@ -4,10 +4,17 @@ output "eks_cluster_name" {
   value = aws_eks_cluster.eks.name
 }
 
-output "ecr_repository_url" {
-  value = aws_ecr_repository.ecr.repository_url
+# Output the repository URL for book_catalog
+output "book_catalog_repository_url" {
+  value = aws_ecr_repository.book_catalog.repository_url
+  description = "The URL of the book_catalog ECR repository"
 }
 
+# Output the repository URL for inventory_management
+output "inventory_management_repository_url" {
+  value = aws_ecr_repository.inventory_management.repository_url
+  description = "The URL of the inventory_management ECR repository"
+}
 output "vpc_id" {
   value = aws_vpc.vpc.id
 }
