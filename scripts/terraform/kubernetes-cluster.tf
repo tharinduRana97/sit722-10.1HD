37aaf4101a72de8051d96605c6917b2b89d2a28a
tharinduRana97 <tharindu.ranasinghe.official@gmail.com>
2024-09-21 21:14:12 +1000

    test 3

--- scripts/terraform/kubernetes-cluster.tf
+++ scripts/terraform/kubernetes-cluster.tf
@@ -19,7 +19,7 @@ module "eks" {
 
   # Reference VPC outputs correctly
   vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.public_subnets
+  subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.public_subnets
 
   # EKS Managed Node Group(s)
