099711cff65d91cff0d506d3816d9dc3d61d4bc4
tharinduRana97 <tharindu.ranasinghe.official@gmail.com>
2024-09-22 09:20:14 +1000

    test commit

--- scripts/terraform/outputs.tf
+++ scripts/terraform/outputs.tf
@@ -51,3 +51,8 @@ output "intra_subnets" {
   value = module.vpc.intra_subnets
   description = "The IDs of the intra subnets"
 }
+
+output "cluster_security_group_id" {
+  description = "Security group ids attached to the cluster control plane."
+  value       = module.eks.cluster_security_group_id
+}
