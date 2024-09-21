39ac924a9714859f9dd4ca7434f4f99aa2b88b83
tharinduRana97 <tharindu.ranasinghe.official@gmail.com>
2024-09-21 11:19:21 +1000

    initial commit

--- scripts/terraform/container-registry.tf
+++ scripts/terraform/container-registry.tf
@@ -1,14 +1,16 @@
-# Lookup the existing ECR repository
+# Check if the ECR repository already exists
 data "aws_ecr_repository" "existing" {
   name = var.container_registry
+}
 
-  # Suppress errors if the repository does not exist
-  depends_on = [aws_ecr_repository.ecr]
+# Determine whether the ECR repository needs to be created
+locals {
+  ecr_repository_exists = length(try(data.aws_ecr_repository.existing.repository_uri, [])) > 0
 }
 
 # Create ECR repository only if it doesn't exist
 resource "aws_ecr_repository" "ecr" {
-  count = length([for repo in try(data.aws_ecr_repository.existing.repository_uri, []): repo]) == 0 ? 1 : 0
+  count = local.ecr_repository_exists ? 0 : 1
   name  = var.container_registry
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
@@ -18,5 +20,5 @@ resource "aws_ecr_repository" "ecr" {
 
 # Output the ECR repository URI, whether it is newly created or already existing
 output "ecr_repository_uri" {
-  value = coalesce(data.aws_ecr_repository.existing.repository_uri, aws_ecr_repository.ecr[0].repository_url)
+  value = local.ecr_repository_exists ? data.aws_ecr_repository.existing.repository_uri : aws_ecr_repository.ecr[0].repository_url
 }
