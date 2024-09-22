9c26a6ab51c73f9935ec8445c396f01934a4d841
tharinduRana97 <tharindu.ranasinghe.official@gmail.com>
2024-09-22 11:29:08 +1000

    Update kubernetes-cluster.tf

--- scripts/terraform/kubernetes-cluster.tf
+++ scripts/terraform/kubernetes-cluster.tf
@@ -3,20 +3,12 @@ module "eks" {
   version = "20.24.1"
 
   cluster_name                   = var.app_name
+  cluster_endpoint_public_access = true
 
-  enable_irsa = true
+  # Use the correct argument for the cluster IAM role
+  iam_role_name = aws_iam_role.eks_cluster_role.name
 
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets
-
-  eks_managed_node_group_defaults = {
-    ami_type       = "AL2_x86_64"
-    instance_types = ["t3.large"]
-    attach_cluster_primary_security_group = true
-  }
-  cluster_iam_role_name          = aws_iam_role.eks_cluster_role.name
-  node_group_iam_role_name       = aws_iam_role.eks_node_group_role.name
-
+  # Node Group configuration
   eks_managed_node_groups = {
     amc-cluster-wg = {
       min_size     = 1
@@ -24,11 +16,16 @@ module "eks" {
       desired_size = 1
       instance_types = ["t3.large"]
       capacity_type  = "SPOT"
+      # Use the correct argument for the node group IAM role
+      iam_role = aws_iam_role.eks_node_group_role.name
       tags = {
         ExtraTag = "helloworld"
       }
     }
   }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
   tags = var.tags 
 }
