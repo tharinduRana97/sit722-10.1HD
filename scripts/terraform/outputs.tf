a2a48781c66d7c9f32196cdc9a0f992f622320c7
tharinduRana97 <tharindu.ranasinghe.official@gmail.com>
2024-09-22 16:12:08 +1000

    adding repositories

--- scripts/terraform/outputs.tf
+++ scripts/terraform/outputs.tf
@@ -17,37 +17,6 @@ output "eks_node_group_role_arn" {
   description = "The ARN of the IAM role for the EKS node group"
 }
 
-# Output the repository URLs for ECR
-output "gateway_repository_url" {
-  value       = aws_ecr_repository.gateway.repository_url
-  description = "The URL of the gateway ECR repository"
-}
-
-output "history_repository_url" {
-  value       = aws_ecr_repository.history.repository_url
-  description = "The URL of the history ECR repository"
-}
-
-output "metadata_repository_url" {
-  value       = aws_ecr_repository.metadata.repository_url
-  description = "The URL of the metadata ECR repository"
-}
-
-output "mock-storage_repository_url" {
-  value       = aws_ecr_repository.mock-storage.repository_url
-  description = "The URL of the mock-storage ECR repository"
-}
-
-output "video-streaming_repository_url" {
-  value       = aws_ecr_repository.video-streaming.repository_url
-  description = "The URL of the video-streaming ECR repository"
-}
-
-output "video-upload_repository_url" {
-  value       = aws_ecr_repository.video-upload.repository_url
-  description = "The URL of the video-upload ECR repository"
-}
-
 # Output the VPC ID
 output "vpc_id" {
   value = module.vpc.vpc_id
