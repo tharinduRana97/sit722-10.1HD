c3d2f776b6a058704b3dc6917aaa20987f72d069
tharinduRana97 <tharindu.ranasinghe.official@gmail.com>
2024-09-22 14:51:59 +1000

    adding repositories

--- scripts/terraform/container-registry.tf
+++ scripts/terraform/container-registry.tf
@@ -1,25 +1,63 @@
-# Create ECR repository for book_catalog
-resource "aws_ecr_repository" "book_catalog" {
-  name                 = "${var.container_registry}-book_catalog"
+# Create ECR repository for gateway
+resource "aws_ecr_repository" "gateway" {
+  name                 = "${var.container_registry}-gateway"
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = true
   }
 
   tags = {
-    Name = "${var.container_registry}-book_catalog"
+    Name = "${var.container_registry}-gateway"
   }
 }
 
-# Create ECR repository for inventory_management
-resource "aws_ecr_repository" "inventory_management" {
-  name                 = "${var.container_registry}-inventory_management"
+# Create ECR repository for history
+resource "aws_ecr_repository" "history" {
+  name                 = "${var.container_registry}-history"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  tags = {
+    Name = "${var.container_registry}-history"
+  }
+}
+
+  # Create ECR repository for metadata
+resource "aws_ecr_repository" "book_catalog" {
+  name                 = "${var.container_registry}-metadata"
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = true
   }
 
   tags = {
-    Name = "${var.container_registry}-inventory_management"
+    Name = "${var.container_registry}-metadata"
+  }
+}
+
+# Create ECR repository for video-streaming
+resource "aws_ecr_repository" "video-streaming" {
+  name                 = "${var.container_registry}-video-streaming"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name = "${var.container_registry}-video-streaming"
+  }
+}
+
+# Create ECR repository for video-upload
+resource "aws_ecr_repository" "video-upload" {
+  name                 = "${var.container_registry}-video-upload"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name = "${var.container_registry}-video-upload"
   }
 }
